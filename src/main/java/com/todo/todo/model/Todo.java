8f1f13457da8f72388786c465ad745dd732d4f01
Nikunj2002 <nikunjsugandhi23@gmail.com>
2025-04-19 19:23:14 +0530

    database created

---
+++ src/main/java/com/todo/todo/model/Todo.java
@@ -0,0 +1,27 @@
+package com.todo.todo.model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class Todo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int todoId;
+	private String title;
+	private String description;
+	private String status;
+	private Date createdAt;
+	private Date taskDate;
+	@ManyToOne
+	private User user;
+}
---
+++ src/main/java/com/todo/todo/model/User.java
@@ -0,0 +1,26 @@
+package com.todo.todo.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	private String userName;
+	private String email;
+	private String password;
+	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+//	@JoinColumn(name="todoId",referencedColumnName = "todoId")
+	private List<Todo> todoList=new ArrayList<>();
+}
